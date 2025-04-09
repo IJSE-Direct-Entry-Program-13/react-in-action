e450fbdfcb4c87c226cab51442c2f26c2555dd91
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 21:17:54 +0530

    Save Customer

--- 11-react-final/src/component/manage-customers/ManageCustomers.tsx
+++ 11-react-final/src/component/manage-customers/ManageCustomers.tsx
@@ -20,58 +20,66 @@ function ManageCustomers() {
 
 export default ManageCustomers;
 
-function Form(){
+function Form() {
     const txtId = useId();
     const txtName = useId();
-    const {register, handleSubmit, reset,
-        formState: {isSubmitted, errors}} =
+    const {
+        register, getValues, handleSubmit, reset,
+        formState: {isSubmitted, errors}
+    } =
         useForm();
 
-    function saveCustomer(){
-        alert("Okay?");
+    async function saveCustomer() {
+        try {
+            await CustomerService
+                .saveCustomer((getValues() as Customer));
+            reset();
+        } catch (err) {
+            alert("Failed to save the customer, try again");
+        }
     }
 
     return (<form className="p-2 px-3"
                   onSubmit={handleSubmit(saveCustomer)}>
         <div className="mb-2">
             <label className="mb-1" htmlFor={txtId}>Customer ID</label>
-            <input {...register('txtId', {
+            <input {...register('id', {
                 pattern: /^C\d{3}$/,
                 required: true
             })}
                    id={txtId} placeholder="Eg. C001"
-                type="text"
-               className={['form-control',
-                   isSubmitted && errors.txtId ? 'is-invalid': ''
-               ].join(' ')}/>
+                   type="text"
+                   className={['form-control',
+                       isSubmitted && errors.id ? 'is-invalid' : ''
+                   ].join(' ')}/>
         </div>
         <div className="mb-3">
             <label className="mb-1" htmlFor={txtName}>Customer Name</label>
-            <input {...register('txtName', {
+            <input {...register('name', {
                 pattern: /^[A-Za-z][A-Za-z ]*$/,
                 required: true
             })}
                    id={txtName} placeholder="Eg. Kasun Sampath"
-                type="text"
+                   type="text"
                    className={['form-control',
-                       isSubmitted && errors.txtName ? 'is-invalid': ''
+                       isSubmitted && errors.name ? 'is-invalid' : ''
                    ].join(' ')}
             />
         </div>
         <div className="d-flex gap-2">
             <button className="btn btn-primary">SAVE</button>
-            <button onClick={()=> reset()} className="btn btn-warning" type="reset">CLEAR</button>
+            <button onClick={() => reset()} className="btn btn-warning" type="reset">CLEAR</button>
         </div>
     </form>)
 }
 
-function Table(){
+function Table() {
 
     const [customerList, setCustomerList] =
         useState<Array<Customer>>([])
 
     useEffect(() => {
-        (async ()=>{
+        (async () => {
             setCustomerList(await CustomerService
                 .getAllCustomers());
         })()
@@ -82,14 +90,17 @@ function Table(){
             <thead>
             <tr>
                 <th className="text-center"
-                    style={{width: '70px'}}>ID</th>
+                    style={{width: '70px'}}>ID
+                </th>
                 <th>NAME</th>
                 <th className="text-center"
-                    style={{width: '70px'}}>DELETE</th>
+                    style={{width: '70px'}}>DELETE
+                </th>
             </tr>
             </thead>
             <tbody>
-            {customerList.map(customer => (<tr>
+            {customerList.map(customer => (<tr
+                key={customer.id}>
                 <td className="text-center">{customer.id}</td>
                 <td>{customer.name}</td>
                 <td className="text-center"><i className='bi-trash'></i></td>
@@ -97,11 +108,11 @@ function Table(){
             </tbody>
             {!customerList.length &&
                 <tfoot>
-                    <tr>
-                        <td colSpan={3} className="text-center">
-                            No Customers Found
-                        </td>
-                    </tr>
+                <tr>
+                    <td colSpan={3} className="text-center">
+                        No Customers Found
+                    </td>
+                </tr>
                 </tfoot>
             }
         </table>

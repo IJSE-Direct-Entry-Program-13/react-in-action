96e180d02e2e08e07442496259078a2df64c1168
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 16:40:14 +0530

    Add menu titles

--- 11-react-final/src/menu/Menu.tsx
+++ 11-react-final/src/menu/Menu.tsx
@@ -6,11 +6,14 @@ function Menu() {
             d-flex border-end justify-content-center">
             <menu className="w-100 text-center list-unstyled m-0
             d-flex flex-column gap-1">
-                <li className="menu__menu-item py-3">
+                <li title="Dashboard"
+                    className="menu__menu-item py-3">
                     <i className="bi-building-dash fs-2"></i></li>
-                <li className="menu__menu-item py-3">
+                <li title="Manage Customers"
+                    className="menu__menu-item py-3">
                     <i className="bi-people fs-2"></i></li>
-                <li className="menu__menu-item py-3">
+                <li title="Manage Items"
+                    className="menu__menu-item py-3">
                     <i className="bi-list fs-2"></i></li>
             </menu>
         </aside>

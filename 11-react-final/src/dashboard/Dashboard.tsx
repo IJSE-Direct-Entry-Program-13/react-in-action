77002641c593337a2220088dec0bd23efea61263
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 18:55:19 +0530

    Design Card

--- 11-react-final/src/dashboard/Dashboard.tsx
+++ 11-react-final/src/dashboard/Dashboard.tsx
@@ -6,11 +6,11 @@ function Dashboard() {
             <header className="text-center p-2 border-bottom">
                 <h1 className="m-0">React Final Project</h1>
             </header>
-            <div className="d-flex">
+            <div className="dashboard__card-wrapper d-flex gap-2 justify-content-around p-3">
                 <Card color="green" text="Customers"
-                      value={20} icon="bi-people" />
-                <Card color="green" text="Items"
-                      value={20} icon="bi-people" />
+                      value={20} icon="bi-people"/>
+                <Card color="blue" text="Items"
+                      value={20} icon="bi-people"/>
             </div>
         </>
     );
@@ -25,8 +25,13 @@ type CardPropType = {
     icon: string
 }
 
-function Card({color, text, value, icon}: CardPropType){
-    return (<>
-
-    </>)
+function Card({color, text, value, icon}: CardPropType) {
+    return (<div className="card text-white p-3 text-center"
+        style={{backgroundColor: color}}>
+        <h2 className="card__title">{text}</h2>
+        <div className="d-flex align-items-center gap-3 justify-content-center">
+            <i className={icon + " card__icon"}></i>
+            <h1 className="card__value">{value}</h1>
+        </div>
+    </div>)
 }

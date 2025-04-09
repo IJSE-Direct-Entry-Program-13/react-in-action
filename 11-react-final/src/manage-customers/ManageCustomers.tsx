6181b623b414768c1a439ee2dd37ffb02d99ce55
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 19:13:26 +0530

    Finish manage-customers form component ui design

--- 11-react-final/src/manage-customers/ManageCustomers.tsx
+++ 11-react-final/src/manage-customers/ManageCustomers.tsx
@@ -1,9 +1,43 @@
 import './ManageCustomers.css';
+import {useId} from "react";
 
 function ManageCustomers() {
     return (
-        <div>Manage Customers works!</div>
+        <div>
+            <header className="p-2 px-3 border-bottom">
+                <h1>Manage Customers</h1>
+                <div>Add, View, Delete Customers</div>
+            </header>
+            <Form/>
+            <Table/>
+        </div>
+
     );
 }
 
 export default ManageCustomers;
+
+function Form(){
+    const txtId = useId();
+    const txtName = useId();
+    return (<form className="p-2 px-3">
+        <div className="mb-2">
+            <label className="mb-1" htmlFor={txtId}>Customer ID</label>
+            <input id={txtId} placeholder="Eg. C001"
+                type="text" className="form-control"/>
+        </div>
+        <div className="mb-3">
+            <label className="mb-1" htmlFor={txtName}>Customer Name</label>
+            <input id={txtName} placeholder="Eg. Kasun Sampath"
+                type="text" className="form-control"/>
+        </div>
+        <div className="d-flex gap-2">
+            <button className="btn btn-primary">SAVE</button>
+            <button className="btn btn-warning" type="reset">CLEAR</button>
+        </div>
+    </form>)
+}
+
+function Table(){
+    return (<>Table works!</>)
+}

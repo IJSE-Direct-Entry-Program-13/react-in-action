1beabbaf310f50cec8ebb72a1134fe0b2fe96361
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 22:05:08 +0530

    Create context shape

--- 11-react-final/src/context/CustomerContext.tsx
+++ 11-react-final/src/context/CustomerContext.tsx
@@ -3,8 +3,13 @@ import {ReactNode} from "react";
 import {CustomerService} from "../service/CustomerService.tsx";
 import {Customer} from "../dto/Customer.tsx";
 
+type CustomerContextType = {
+    getAllCustomers: ()=> Promise<Array<Customer>>,
+    customerStore: Array<Customer>
+}
+
 export const CustomerContext
-    = createContext({});
+    = createContext<CustomerContextType | null>(null);
 
 function customerReducer(prevState:Customer[],
                          currentAction: {type: string,
@@ -20,7 +25,7 @@ export function CustomerProvider({children}:{children:ReactNode}){
     const [customerStore, dispatchFn] =
         useReducer(customerReducer, []);
     return (<CustomerContext value={{getAllCustomers,
-        customerStore, dispatchFn}}>
+        customerStore}}>
         {children}
     </CustomerContext>)
 }

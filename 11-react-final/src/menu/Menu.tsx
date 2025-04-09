cf7ec50183db82d1adc26509eaa7bd48646790a9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 18:10:56 +0530

    Add router links

--- 11-react-final/src/menu/Menu.tsx
+++ 11-react-final/src/menu/Menu.tsx
@@ -1,18 +1,24 @@
 import './Menu.css';
+import {useNavigate} from "react-router";
 
 function Menu() {
+    const navigate = useNavigate();
+
     return (
         <aside className="menu vh-100
             d-flex border-end justify-content-center">
             <menu className="w-100 text-center list-unstyled m-0
             d-flex flex-column gap-1">
-                <li title="ManageItems"
+                <li onClick={()=>navigate('/dashboard')}
+                    title="Dashboard"
                     className="menu__menu-item py-3">
                     <i className="bi-building-dash fs-2"></i></li>
-                <li title="Manage Customers"
+                <li onClick={()=>navigate('/manage-customers')}
+                    title="Manage Customers"
                     className="menu__menu-item py-3">
                     <i className="bi-people fs-2"></i></li>
-                <li title="Manage Items"
+                <li onClick={()=>navigate('/manage-items')}
+                    title="Manage Items"
                     className="menu__menu-item py-3">
                     <i className="bi-list fs-2"></i></li>
             </menu>

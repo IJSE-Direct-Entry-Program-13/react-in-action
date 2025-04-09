13fa571108cde640fbfc3e5e7e658532f597a589
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 19:03:18 +0530

    Finish dashboard UI design

--- 11-react-final/src/dashboard/Dashboard.tsx
+++ 11-react-final/src/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import './Dashboard.css';
+import {useNavigate} from "react-router";
 
 function Dashboard() {
     return (
@@ -7,10 +8,12 @@ function Dashboard() {
                 <h1 className="m-0">React Final Project</h1>
             </header>
             <div className="dashboard__card-wrapper d-flex gap-2 justify-content-around p-3">
-                <Card color="green" text="Customers"
+                <Card color="lightgreen" text="Customers"
+                      link="/manage-customers"
                       value={20} icon="bi-people"/>
-                <Card color="blue" text="Items"
-                      value={20} icon="bi-people"/>
+                <Card color="lightblue" text="Items"
+                      link="/manage-items"
+                      value={40} icon="bi-list"/>
             </div>
         </>
     );
@@ -22,13 +25,17 @@ type CardPropType = {
     color: string,
     text: string,
     value: number,
-    icon: string
+    icon: string,
+    link: string
 }
 
-function Card({color, text, value, icon}: CardPropType) {
-    return (<div className="card text-white p-3 text-center"
+function Card({color, text, value, icon, link}: CardPropType) {
+    const navigate = useNavigate();
+
+    return (<div onClick={()=> navigate(link)}
+                 className="card p-3 text-center"
         style={{backgroundColor: color}}>
-        <h2 className="card__title">{text}</h2>
+        <h1 className="card__title">{text}</h1>
         <div className="d-flex align-items-center gap-3 justify-content-center">
             <i className={icon + " card__icon"}></i>
             <h1 className="card__value">{value}</h1>

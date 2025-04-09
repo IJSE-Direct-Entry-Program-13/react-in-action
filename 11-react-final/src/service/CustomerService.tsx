c0715cca77f381f9488a488c799e4f9292d6b494
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 20:36:45 +0530

    Finish CustomerService

--- 11-react-final/src/service/CustomerService.tsx
+++ 11-react-final/src/service/CustomerService.tsx
@@ -1,15 +1,20 @@
+import { Customer } from "../dto/Customer";
+import {http} from "../util/HttpClient.tsx";
+
 export class CustomerService {
 
-    static async getAllCustomers(){
-        
+    static async getAllCustomers(): Promise<Array<Customer>>{
+        const response =
+            await http.get('/customers');
+        return response.data;
     }
 
-    static async saveCustomer(){
-
+    static async saveCustomer(customer: Customer){
+        await http.post('/customers', customer);
     }
 
-    static async deleteCustomer(){
-
+    static async deleteCustomer(id: string){
+        await http.delete(`/customers/${id}`);
     }
 
 }

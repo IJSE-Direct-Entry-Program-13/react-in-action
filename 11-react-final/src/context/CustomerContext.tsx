dcd02c140d69e4a37286a32918197237dee4526d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-09 21:55:20 +0530

    Revamp CustomerContext with Reducer

--- 11-react-final/src/context/CustomerContext.tsx
+++ 11-react-final/src/context/CustomerContext.tsx
@@ -1,11 +1,26 @@
-import {createContext} from "react";
+import {createContext, useReducer} from "react";
 import {ReactNode} from "react";
+import {CustomerService} from "../service/CustomerService.tsx";
+import {Customer} from "../dto/Customer.tsx";
 
 export const CustomerContext
     = createContext({});
 
+function customerReducer(prevState:Customer[],
+                         currentAction: {type: string,
+                             customerList: Customer[]}): Customer[]{
+    return prevState;
+}
+
+export async function getAllCustomers(){
+    return await CustomerService.getAllCustomers();
+}
+
 export function CustomerProvider({children}:{children:ReactNode}){
-    return (<CustomerContext value={{}}>
+    const [customerStore, dispatchFn] =
+        useReducer(customerReducer, []);
+    return (<CustomerContext value={{getAllCustomers,
+        customerStore, dispatchFn}}>
         {children}
     </CustomerContext>)
 }
